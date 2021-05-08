6d7c8b8dfda9fbe2326af0dffcb2853b855f9e26
satyam52 <satyam0350@gmail.com>
2021-05-08 20:40:38 +0530

    added story by id and pagination logic

--- app.py
+++ app.py
@@ -5,23 +5,26 @@ from datetime import datetime
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen as uOpen
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 import os
 from firebase_admin import credentials, firestore, initialize_app
 from flask_cors import CORS
 from covid_diaries_nlp import CovidDiariesNLP
+import pymongo
 
 app = Flask(__name__)
 CORS(app)
 
 # initialize database
 try:
-    cred = credentials.Certificate('key.json')
-    default_app = initialize_app(cred)
-    db = firestore.client()
-    db_ref = db.collection('stories')
-    print("Successful DataBase connection")
+    dbConn = pymongo.MongoClient(
+        "mongodb+srv://Abhishek:Satyam03@covid-diaries-cluster.mwtcw.mongodb.net/<dbname>?retryWrites=true&w=majority")
+    dbConn.server_info()
+    db = dbConn['Stories']
+    collection = db['StoriesCollection']
+    print("DateBase Connected !!!")
 except Exception as e:
-    print(f"An Error Occured: {e}")
+    print(f'An Error in Database : {e}')
 
 
 # adding story(using scheduler)
@@ -41,9 +44,26 @@ def index():
     for i in eachNews:
         allNewsLinks.append(i.a['href'])
 
+    # checking curated page 1 and 2
+    for i in [1, 2]:
+        newsUrl = f"https://timesofindia.indiatimes.com/etimeslistgenric.cms?msid=2886704&query=mycoronastory&datasection=lifestyle&curpg={i}"
+        req = Request(newsUrl, headers={'User-Agent': 'Mozilla/5.0'})
+        newsPage = uOpen(req).read()
+        newsPageHtml = bs(newsPage, "html.parser")
+        eachNews = newsPageHtml.findAll("span", {"class": "md_news_left"})
+
+        for i in eachNews:
+            allNewsLinks.append(i.a['href'])
+
     allStoryLinksInDB = []
-    for doc in db_ref.stream():
-        allStoryLinksInDB.append(doc.to_dict()['link'])
+
+    try:
+        allStory = collection.find({}, {"link": 1, "_id": 0})
+    except Exception as e:
+        return f"link fetching error : {e}", 400
+
+    for i in allStory:
+        allStoryLinksInDB.append(i['link'])
 
     saveCount = 0
     passCount = 0
@@ -61,10 +81,11 @@ def index():
                 singleNewsPageHtml = bs(req.text, "html.parser")
                 heading = singleNewsPageHtml.find(
                     "arttitle").text
-                content = singleNewsPageHtml.find(
-                    "div", {"class": "Normal"}).text
-                garbage = "Did you fight COVID-19? We want to hear all about it. ETimes Lifestyle is calling all the survivors of COVID to share their stories of survival and hope.Write to us at toi.health1@gmail.com with 'My COVID story' in the subject lineWe will publish your experience.DISCLAIMERThe views expressed in this article should not be considered as a substitute for a physician's advice. Please consult your treating physician for more"
-                content = content.replace(garbage, "")
+                contentHTML = singleNewsPageHtml.find(
+                    "div", {"class": "Normal"})
+                contentText = contentHTML.text
+                garbageText = "Did you fight COVID-19? We want to hear all about it. ETimes Lifestyle is calling all the survivors of COVID to share their stories of survival and hope.Write to us at toi.health1@gmail.com with 'My COVID story' in the subject lineWe will publish your experience.DISCLAIMERThe views expressed in this article should not be considered as a substitute for a physician's advice. Please consult your treating physician for more"
+                contentText = contentText.replace(garbageText, "")
                 date = singleNewsPageHtml.find('div', {"class": "as_byline"})
                 Fdate = date.find('div', attrs={'dateval': True})
                 rawDate = Fdate['dateval']
@@ -73,13 +94,23 @@ def index():
                 stdDate = datetime.strptime(
                     cleanDate, '%b %d %Y %H:%M')
                 title = heading.replace('"', "").replace("\n", "")
-                content = content.replace('"', "").replace("\n", "")
+                contentText = contentText.replace('"', "").replace("\n", "")
                 cvnlp = CovidDiariesNLP()
-                dataTags = cvnlp.getNLPInfo(text=content)
-
+                dataTags = cvnlp.getNLPInfo(text=contentText)
+                # garbageHTML = """ <strong><em>Did you fight COVID-19? We want to hear all about it. ETimes Lifestyle is calling all the survivors of COVID to share their stories of survival and hope.<div class="last7brdiv"></div><br/><div class="last6brdiv"></div><br/>Write to us at toi.health1@gmail.com with 'My COVID story' in the subject line<div class="last5brdiv"></div><br/><div class="top2brdiv"></div><br/>We will publish your experience.<div class="last3brdiv"></div><br/>DISCLAIMER<div class="last1brdiv"></div><br/><br/>The views expressed in this article should not be considered as a substitute for a physician's advice. Please consult your treating physician for more details.</em></strong>"""
+                # garbage = bs(garbageHTML, 'html.parser')
+                strongTags = contentHTML.find_all('strong')
+                try:
+                    strongTags[1].decompose()
+                except Exception as e:
+                    print(e)
+                # contentHTML = contentHTML.strongTags[1]
+                # print(contentHTML)
+                contentHTML = str(contentHTML).replace(
+                    '<br>', "<br/>")
                 data = {
                     'title': title,
-                    'content': content,
+                    'content': contentHTML,
                     'link': link,
                     'source': "TIMESOFINDIA",
                     'dateTime': stdDate,
@@ -88,7 +119,7 @@ def index():
                     'abusive': dataTags['abusive'],
                     'emotions': dataTags['emotions']
                 }
-                db_ref.document().set(data)
+                collection.insert_one(data)
                 print(f"Saved {saveCount} succesfully")
                 saveCount += 1
             except Exception as e:
@@ -96,22 +127,57 @@ def index():
     return jsonify({"success": True}), 200
 
 
-# Return all stories
-@app.route('/', methods=['GET', 'POST'])
-def read():
-    if request.method == 'POST':
-        order = request.json['sortByDate']
-    else:
-        order = 'newest'
+# total count stories for pagination
+@app.route("/paginate", methods=["GET"])
+def pagination():
     try:
-        allStory = [doc.to_dict()
-                    for doc in db_ref.order_by('dateTime').stream()]
-        if order == 'newest':
-            resStory = allStory[::-1]
-        else:
-            resStory = allStory
+        totalArticleCount = collection.estimated_document_count()
+        return jsonify({"totalArticleCount": totalArticleCount}), 200
 
-        return jsonify(resStory), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+# Return all stories
+PER_PAGE = 10
+@app.route('/', methods=['GET'])
+def readAll():
+    order = request.args.get('date', 'newest')
+    page = int(request.args.get('pg', 1))
+
+    try:
+        totalArticleCount = collection.estimated_document_count()
+        if order == 'newest':
+            allStory = collection.find().sort(
+                [('dateTime', pymongo.DESCENDING)]).skip(PER_PAGE*(page-1)).limit(PER_PAGE)
+        else:
+            allStory = collection.find().sort(
+                [('dateTime', pymongo.ASCENDING)]).skip(PER_PAGE*(page-1)).limit(PER_PAGE)
+
+        # # returning 10 stories per page
+        resStory = []
+        # resStory = resStory[(page-1)*PER_PAGE:page*PER_PAGE]
+        for i in allStory:
+            resStory.append(i)
+        # print(resStory)
+        return dumps(resStory), 200
+
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+# Return single story by id
+@app.route('/id=<_id>', methods=['GET'])
+def story(_id):
+    # id = request.args.get('id')
+    if _id in (None, ''):
+        return f"Article Id not found", 400
+    try:
+        story = collection.find_one({'_id': ObjectId(_id)})
+        if not story:
+            return f"Article not found", 404
+
+        return dumps(story), 200
 
     except Exception as e:
         return f"An Error Occured: {e}"
